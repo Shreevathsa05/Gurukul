e65005a276eb3457a8158694f0fd6d166108754f
Shreevathsa05 <bhatshreevathsa17@gmail.com>
2025-09-27 13:54:17 +0530

    integrated and sizing

--- frontend/src/pages/MiniMeet.jsx
+++ frontend/src/pages/MiniMeet.jsx
@@ -6,9 +6,10 @@ function MiniMeet() {
     src='https://mini-meet-prince.vercel.app'
     title='MiniMeet'
     width='100%'
-    height='100%'
+    height='130%'
     style={{ border: 'none', position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}
     allow='camera; microphone; fullscreen; speaker; display-capture'
+    allowFullScreen
     >
       
     </iframe>
